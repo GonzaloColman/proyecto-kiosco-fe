346ea2be49c7fa1264a9a2789599ad2051d637d5
gonzalo <gonzalogabrielapu@gmail.com>
2024-06-03 20:30:38 -0300

    correccion login

--- src/app/paginas/newaccount/page.tsx
+++ src/app/paginas/newaccount/page.tsx
@@ -10,53 +10,34 @@ export default function NewAccount() {
 		<main >
 			<Form>
 				<Row className="mb-3">
-					<Form.Group as={Col} controlId="formGridEmail">
-						<Form.Label>Email</Form.Label>
+					<Form.Group className="mb-3" controlId="formNombre">
+						<Form.Label>Nombre</Form.Label>
+						<Form.Control placeholder="Nombre completo" />
+					</Form.Group>
+
+					<Form.Group as={Col} controlId="formEmail">
+						<Form.Label>Correo</Form.Label>
 						<Form.Control type="email" placeholder="Enter email" />
 					</Form.Group>
 
-					<Form.Group as={Col} controlId="formGridPassword">
-						<Form.Label>Password</Form.Label>
-						<Form.Control type="password" placeholder="Password" />
+					<Form.Group as={Col} controlId="formPassword">
+						<Form.Label>Contraceña</Form.Label>
+						<Form.Control type="password" placeholder="Contraceña" />
+					</Form.Group>
+
+					<Form.Group as={Col} controlId="formPassword2">
+						<Form.Label>Repite Contraceña</Form.Label>
+						<Form.Control type="password" placeholder="Contraceña" />
 					</Form.Group>
 				</Row>
 
-				<Form.Group className="mb-3" controlId="formGridAddress1">
-					<Form.Label>Address</Form.Label>
-					<Form.Control placeholder="1234 Main St" />
-				</Form.Group>
-
-				<Form.Group className="mb-3" controlId="formGridAddress2">
-					<Form.Label>Address 2</Form.Label>
-					<Form.Control placeholder="Apartment, studio, or floor" />
-				</Form.Group>
-
-				<Row className="mb-3">
-					<Form.Group as={Col} controlId="formGridCity">
-						<Form.Label>City</Form.Label>
-						<Form.Control />
-					</Form.Group>
-
-					<Form.Group as={Col} controlId="formGridState">
-						<Form.Label>State</Form.Label>
-						<Form.Select defaultValue="Choose...">
-							<option>Choose...</option>
-							<option>...</option>
-						</Form.Select>
-					</Form.Group>
-
-					<Form.Group as={Col} controlId="formGridZip">
-						<Form.Label>Zip</Form.Label>
-						<Form.Control />
-					</Form.Group>
-				</Row>
-
-				<Form.Group className="mb-3" id="formGridCheckbox">
-					<Form.Check type="checkbox" label="Check me out" />
+				<Form.Group className="mb-3" controlId="formDireccion">
+					<Form.Label>Direccion</Form.Label>
+					<Form.Control placeholder="calle altura." />
 				</Form.Group>
 
 				<Button variant="primary" type="submit">
-					Submit
+					Registrar
 				</Button>
 			</Form>
 
