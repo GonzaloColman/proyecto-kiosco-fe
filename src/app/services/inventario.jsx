ec7c903d8503b33cc232fca260bef9a961240dc7
Gonzalo <gonzalogabrielapu@gmail.com>
2024-07-15 12:04:49 -0300

    a

--- src/app/services/inventario.jsx
+++ src/app/services/inventario.jsx
@@ -1,7 +1,11 @@
-export clientAxios from './axios.js'
+export { clienteAxios } from './axios'
 
-export const getInventario = ()=>{
+export const getInventario = async () => {
   try {
-    const response = Promise.resolve(clienteAxios.get())
+    const response = await clienteAxios.get(); // Espera a que se resuelva la promesa
+    
+  } catch (error) {
+    // Manejo del error
+    console.error('Error al obtener el inventario:', error);
   }
-}
+};
