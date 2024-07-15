4a3d398014ff449e9cdc2f3d6496acd43af63deb
Gonzalo <gonzalogabrielapu@gmail.com>
2024-07-15 08:21:13 -0300

    header

---
+++ src/app/components/header_footer/header.tsx
@@ -0,0 +1,14 @@
+'use client'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+export default function Header() {
+	return (
+		<div >
+			<div className='titulo'>
+				<h1>Kiosco</h1>
+				<h4>venta y organizacion de stock</h4>
+			</div>
+		</div>
+	);
+}
